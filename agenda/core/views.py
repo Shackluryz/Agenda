ec6577fd9e8ffb8d714fee3ab53ce1fcf2d2f190
Lucas Rodrigues <43302793+Shackluryz@users.noreply.github.com>
2022-07-08 08:48:55 -0300

    [ UPDATE ] Implementa forma alternativa de redirecionamento

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from core.models import Evento
 
 # Create your views here.
-def index(request):
-  return redirect('/agenda/')
+# def index(request):
+#   return redirect('/agenda/')
 
 def lista_eventos(request):
   evento = Evento.objects.all()
