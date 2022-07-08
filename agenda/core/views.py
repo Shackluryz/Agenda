49bd6fd69c18d3560a9f84f1e0a92332dd246055
Lucas Rodrigues <43302793+Shackluryz@users.noreply.github.com>
2022-07-08 01:14:24 -0300

    [ ADD ] Prepara template de pagina

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -3,8 +3,7 @@ from core.models import Evento
 
 # Create your views here.
 def lista_eventos(request):
-  usuario = request.user
-  evento = Evento.objects.filter(usuario=usuario)
+  evento = Evento.objects.all()
   dados =  {'eventos':evento}
   return render(request, 'agenda.html', dados)
 
