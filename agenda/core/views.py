d20a9f92fc864ffec968bbc6d4106065190f3ca2
Lucas Rodrigues <43302793+Shackluryz@users.noreply.github.com>
2022-07-14 23:14:34 -0300

    [ ADD ] Implementa restrição de acesso

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 # def index(request):
 #   return redirect('/agenda/')
 
+@login_required()
 def lista_eventos(request):
   evento = Evento.objects.all()
   dados =  {'eventos':evento}
